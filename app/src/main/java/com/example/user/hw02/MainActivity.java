f1a69b27cead9ddbb4e74a2d594ebcf70048c08b
alexwang0405 <alexwang0405@gmail.com>
2016-03-22 17:33:40 +0800

    first

---
+++ app/src/main/java/com/example/user/hw02/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.user.hw02;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId())
+        {
+            case R.id.action_about:
+                AlertDialog.Builder ad = new  AlertDialog.Builder(this);
+                ad.setTitle("about this app");
+                ad.setMessage("作者:王柏傑");
+
+                DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface di, int i)
+                    {
+
+                    }
+                };
+
+                ad.setPositiveButton("確定", listener);
+                ad.show();
+                break;
+
+            case R.id.action_reset:
+
+                break;
+
+            case R.id.action_settings:
+
+                break;
+
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
